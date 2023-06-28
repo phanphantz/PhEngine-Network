ebaa2e19f9b75e143c773c7475fc9ed70e8ed4e9
Phun Peeticharoenthum <phunphun484@gmail.com>
2023-06-29 01:52:09 +0700

    FEAT: 3 Types of API classes

--- Runtime/Common/API.cs
+++ Runtime/Common/API.cs
@@ -1,27 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
 using PhEngine.Core.Operation;
 using PhEngine.JSON;
 
 namespace PhEngine.Network
 {
     [Serializable]
-    public abstract class API<T> : API
+    public abstract class RespondListAPI<T> : API
     {
+        public event Action<List<T>> onReceiveDataList;
         public override APIOperation Create()
         {
             var apiOp = base.Create();
-            apiOp.Expect(OnReceiveData, CreateMockedData());
+            apiOp.ExpectList
+            (
+                dataList =>
+                {
+                    OnReceiveDataList(dataList);
+                    onReceiveDataList?.Invoke(dataList);
+                }, 
+                CreateMockedListData()
+            );
+            return apiOp;
+        }
+
+        public abstract void OnReceiveDataList(List<T> result);
+        public virtual List<T> CreateMockedListData() => default;
+    }
+    
+    [Serializable]
+    public abstract class RespondSingleAPI<T> : API
+    {
+        public event Action<T> onReceiveData;
+        public override APIOperation Create()
+        {
+            var apiOp = base.Create();
+            apiOp.Expect
+            ((data) =>
+                {
+                    OnReceiveData(data);
+                    onReceiveData?.Invoke(data);
+                }, 
+                CreateMockedData()
+            );
             return apiOp;
         }
 
         public abstract void OnReceiveData(T result);
+        public virtual T CreateMockedData() => default;
+    }
 
-        protected virtual T CreateMockedData() => default;
+    [Serializable]
+    public abstract class RespondJsonAPI : API
+    {
+        public event Action<JSONObject> onSuccess;
+        public override APIOperation Create()
+        {
+            var apiOp = base.Create();
+            apiOp.OnSuccess += (result) =>
+            {
+                OnReceiveJson(result.dataJson);
+                onSuccess?.Invoke(result.dataJson);
+            };
+            return apiOp;
+        }
+
+        public abstract void OnReceiveJson(JSONObject result);
     }
 
     [Serializable]
     public abstract class API
     {
+        public event Action<ServerResult> onFail;
         public abstract WebRequestForm Form { get; }
         public void Call()
         {
@@ -33,13 +83,15 @@ namespace PhEngine.Network
         {
             var apiOp = APICaller.Instance.Create(Form, CreateBody());
             apiOp.SetMockedResponse(CreateBody());
-            apiOp.OnSuccess += (result) => OnReceiveJson(result.dataJson);
-            apiOp.OnFail += OnFail;
+            apiOp.OnFail += (result) =>
+            {
+                OnFail(result);
+                onFail?.Invoke(result);
+            };
             return apiOp;
         }
 
         public abstract void OnFail(ServerResult result);
-        public virtual void OnReceiveJson(JSONObject result) {}
 
         protected virtual JSONObject CreateMockJson() => null;
         public virtual JSONObject CreateBody() => null;
