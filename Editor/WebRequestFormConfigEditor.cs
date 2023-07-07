a2545c5d1c4a7c9d020e6f5b42cd0528d2329eba
Phun Peeticharoenthum <phunphun484@gmail.com>
2023-07-07 15:17:32 +0700

    FEAT: Test button for WebRequestFormConfig

---
+++ Editor/WebRequestFormConfigEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace PhEngine.Network.Editor
+{
+    [CustomEditor(typeof(WebRequestFormConfig))]
+    public class WebRequestFormConfigEditor : UnityEditor.Editor
+    {
+        WebRequestFormConfig config;
+        
+        void OnEnable()
+        {
+            config = target as WebRequestFormConfig;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Test", GUILayout.Width(50), GUILayout.Height(25)))
+                config.Test();
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
